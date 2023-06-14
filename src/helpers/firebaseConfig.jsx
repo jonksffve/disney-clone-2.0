97d8ae149a8b6a455ed1636659da23635b917415
Jonathan Ramirez <jonksff@gmail.com>
2023-06-14 16:41:50 -0400

    Installed firebase

---
+++ src/helpers/firebaseConfig.jsx
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+	apiKey: 'AIzaSyAO7gGaoHW__X_a3E5Lbr34Vo3WrMNkAoA',
+	authDomain: 'disneyplus-clone-fd7f2.firebaseapp.com',
+	databaseURL: 'https://disneyplus-clone-fd7f2-default-rtdb.firebaseio.com',
+	projectId: 'disneyplus-clone-fd7f2',
+	storageBucket: 'disneyplus-clone-fd7f2.appspot.com',
+	messagingSenderId: '132728618748',
+	appId: '1:132728618748:web:be2f708d8b5b20f613a179',
+	measurementId: 'G-4JH24LT5HP',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
